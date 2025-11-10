67c896ce2c9b0503a5ec511cdd384f66fb6d9bdd
samueljkauff <samueljkauff@gmail.com>
2025-11-10 00:28:39 -0500

    Installed prisma, started dockerizing

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 
+const pool =  require('./db');
 const app = express();
 const PORT = 2121;
 
+app.use(express.json());
+app.get('api/set-up', async (resizeBy, req) => {
+    try{
+        await pool.query('CREATE TABLE USERS()')
+    } catch {
+
+    }
+});
 
 app.listen(PORT, () => console.log(`Server has started on ${ PORT }`));
