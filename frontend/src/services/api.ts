b5de68c9b229bb6cf7fc981c47e35d0bdd70400e
samueljkauff <samueljkauff@gmail.com>
2025-11-13 13:57:21 -0500

    route change to start with api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import type { Token } from "../interfaces/token_interface";
 
 export const createToken = async (data: Token) => {
-    const res = await fetch("http://localhost:2121/token/create", {
+    const res = await fetch("http://localhost:2121/api/token/create", {
         method: "POST",
         headers: { "Content-Type": "appliction/json"},
         body: JSON.stringify({ data }),
